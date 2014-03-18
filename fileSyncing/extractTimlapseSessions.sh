d897f1bcc85a0adab43e8e648e60c8b4ab24dcd2
Sam Rendall <sdrendall@gmail.com>
2014-03-18 19:30:19 -0400

    script to sort and manage timelapse images

--- fileSyncing/extractTimlapseSessions.sh
+++ fileSyncing/extractTimlapseSessions.sh
@@ -1,29 +1,33 @@
 #! /bin/bash
 
-dumpPath=""
-startPath=""
+dumpPath="/home/sam/code/conditioningCage/fileSyncing"
+startPath="/home/sam/code/conditioningCage/fileSyncing/test"
 
-ensureDumpDir() {
-    fullPath="$dumpPath/$1"
-    if [! -d $fullPath]
+ensureDir() {
+    if [ ! -d $1 ]
         then
-        mkdir $fullPath
+        mkdir $1
     fi
 }
 
 relocateImages() {
-    for im in *
+    # Extract distinct timelapses
+    for dateAndTimeStamp in $(ls *.jpg | cut -d'_' -f2,3 | sort | uniq)
     do
-        
+        currDumpDir="$1/$dateAndTimeStamp"
+        ensureDir $currDumpDir 
+        rsync -avz *$dateAndTimeStamp* $currDumpDir
+    done
 }
 
-for pi in rasPis
+for pi in *
 do
     if [ -d $pi ]
         then
         pushd $pi
-        ensureDumpDir $pi
-        relocateImages $pi
+        piDumpPath="$dumpPath/$pi"
+        ensureDir $piDumpPath
+        relocateImages $piDumpPath
         popd
     fi
 done
