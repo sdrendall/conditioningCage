21a4269a260d43e48e6981e90f4e2f7076adbd26
Sam Rendall <sdrendall@gmail.com>
2014-05-05 19:02:34 -0400

    expand user ~

--- Python/cameraControls.py
+++ Python/cameraControls.py
@@ -270,6 +270,7 @@ class Timelapse(CameraState):
     def captureImage(self):
         filename = "~/timelapse/{cageName}_{dateTime}_%05d.jpg" % self.getNextImageNumber()
         filename = filename.format(**self)
+        filename = os.path.expanduser(filename)
         self.camera.capture(filename, resize=(self['width'],self['height']), quality=self['jpegQuality'])
 
     def getNextImageNumber(self):
