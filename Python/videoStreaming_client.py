077d9ebc3f4c4c6130c8a31574daaed3bb213e11
Sam Rendall <sdrendall@gmail.com>
2014-06-03 11:46:24 -0400

    args list now begins with the processns name

--- Python/videoStreaming_client.py
+++ Python/videoStreaming_client.py
@@ -25,7 +25,8 @@ class RaspiVidProtocol(protocol.ProcessProtocol):
 
 
 class VideoStreamingProtocol(protocol.Protocol):
-    rpiVidArgs = (' -t', '60000',
+    rpiVidArgs = ('raspiVid'
+        '-t', '60000',
         '-fps', '30',
         '-cfx', '128:128',
         '-b', '3000000',
